3a4f4a09761127b7bf4d88ef932e12d4bc21922d
Huste6 <quannghihai1@gmail.com>
2024-09-21 22:10:31 +0700

    fix phần applicants

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../share/Navbar'
 import ApplicantsTable from './ApplicantsTable'
 import { useParams } from 'react-router-dom'
@@ -9,14 +9,15 @@ import { setAllApplicants } from '../redux/applicantionSlice'
 
 const Applicants = () => {
   const params = useParams();
+  const [numberApplicants,setNumberApplicants] = useState(0);
   const dispatch = useDispatch();
   useEffect(() => {
     const fetchAllApplicants = async () => {
       try {
         const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
-        console.log(res.data)
         if (res.data.success) {
           dispatch(setAllApplicants(res.data.job))
+          setNumberApplicants(res.data.job.applications.length)
         }
       } catch (error) {
         console.error(error);
@@ -28,7 +29,7 @@ const Applicants = () => {
     <div>
       <Navbar />
       <div className='max-w-7xl mx-auto'>
-        <h1 className='font-bold text-xl my-5'>Applicants (3)</h1>
+        <h1 className='font-bold text-xl my-5'>Applicants ({numberApplicants})</h1>
         <ApplicantsTable />
       </div>
     </div>
